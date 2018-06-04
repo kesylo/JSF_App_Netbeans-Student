1d43ca1fafd8c62a6308f0795974b19baa165aeb
kesylo <32457792+kesylo@users.noreply.github.com>
2018-06-04 02:06:39 +0200

    auth UI done

---
+++ src/java/managedBeanPackage/beanLogin.java
@@ -0,0 +1,15 @@
+
+package managedBeanPackage;
+
+public class beanLogin {
+
+    public beanLogin() {}
+    
+    public String LoginProcess() {
+        String cle = "none";
+     // verif si id et pass sont bon
+
+        return cle;
+    }
+    
+}
